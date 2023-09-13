e95c4315a2db6369e3ac32f70737f3829e2d776e
Jsow <jolu032302@gmail.com>
2023-09-13 13:47:42 +0300

    Update 1.4

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -4,9 +4,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        cacheTime: Infinity,
+      },
+    },
+});
+
 const App = () => {
     return (
         <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
             <header>
                 <Link to="/">Adopt Me!</Link>
             </header>
@@ -14,6 +24,7 @@ const App = () => {
                 <Route path="/details/:id" element={<Details />} />
                 <Route path="/" element={<SearchParams />} />
             </Routes>
+        </QueryClientProvider>
         </BrowserRouter>
     );
 };
